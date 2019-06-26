31a3bb03c82b734020d28488488d410abc596eea
Stephen Cefali <scefali@sentry.io>
2019-06-26 15:44:29 -0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const path = require('path');
 let app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+ 
+
 const port = process.env.PORT || '8080';
 
 app.set('port', port);
